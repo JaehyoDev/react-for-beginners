34914dd0965c6ec7ce28c9410b1446662b2c3e5b
JH <67933778+jayhigh47@users.noreply.github.com>
2022-12-11 22:10:44 +0900

    React: 6.4 Effects Cleanup

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,21 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((counter) => counter + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("I run all the time.");
-
+function Hello() {
   useEffect(() => {
-    console.log("I run only once. CALL THE API...");
+    console.log("Hi :)");
+    return () => console.log("bye :(");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' or 'keyword' changes.");
-  }, [counter, keyword]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="search"
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
